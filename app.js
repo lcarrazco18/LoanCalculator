3272800a9e5fcad88048143bdf5e455e5d52901d
Luis Carrazco <luiscarrazco@gmail.com>
2019-05-19 10:15:30 -0600

    Calculate Vars

---
+++ app.js
@@ -0,0 +1,12 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+    console.log('Calculating...');
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+
+    e.preventDefault();
+}
